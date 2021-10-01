b93df745cb7bdb9c1a388c783b33f1748aef9518
SzymonRudnikowski <34752067+SzymonRudnikowski@users.noreply.github.com>
2021-10-01 22:51:19 +0200

    Optimalization changes
    deleted ban and kick, updated info for future dev.

--- commands/info.js
+++ commands/info.js
@@ -5,5 +5,21 @@ module.exports = {
     execute(message, args) {
         message.channel.send(`**Server name:** ${message.guild.name}`);
         message.channel.send(`**Server online:** ${message.guild.available}`);
+
+        const embed = new Discord.MessageEmbed()
+        // Kolor 
+        .setColor('0x03f4fc')
+        // Tytul 
+        .setTitle('title')
+        // Tekst, enter = \n
+        .setDescription('text')
+        /// Wiele pol do dodania (opcjonalne - zakomendtowane)
+        // .addFields(
+        //     {name: '', value: ''},
+        //     {name: '', value: ''}
+        // )
+        .setFooter('PMB');
+
+        message.channel.send(embed);
     },
 };
