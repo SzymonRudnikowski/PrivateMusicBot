5140a8c7a363841c5896d80708d192f90a0a9a82
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-01 21:27:09 +0200

    channel message changed

--- commands/skip.js
+++ commands/skip.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: ['sk'],
     decription: "skips song that is currently playing in the queue",
     async execute(message, server_queue) {
-        if (!message.member.voice.channel) return message.channel.send('**You need to be in a channel to execute this command!**');
+        if (!message.member.voice.channel) return message.channel.send(`${message.author} **You need to be in a channel to execute this command!**`);
         if(!server_queue){
             console.log('Skip command while not in channel.')
             return message.channel.send(`**There are no songs in queue ??**`);
--- commands/stop.js
+++ commands/stop.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message, server_queue) {
         if (!message.member.voice.channel) {
             console.log('Stop song command while not in channel.')
-            return message.channel.send('**You need to be in a channel to execute this command!**');
+            return message.channel.send(`${message.author} **You need to be in a channel to execute this command!**`);
         }
         server_queue.songs = [];
         server_queue.connection.dispatcher.end();
