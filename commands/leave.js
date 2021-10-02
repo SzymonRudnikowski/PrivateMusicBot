a97e88e358bb163e739f4254d15f7cd11138d108
SzymonRudnikowski <34752067+SzymonRudnikowski@users.noreply.github.com>
2021-10-03 00:51:05 +0200

    Leave.js fix
    Fixed no queue problem.

--- commands/leave.js
+++ commands/leave.js
@@ -5,13 +5,16 @@ module.exports = {
     aliases: [],
     decription: "stops the song that is currently playing in the queue",
     async execute(message, args) {
-
+        const voice_channel = message.member.voice.channel;
         if (!message.member.voice.channel) {
             console.log('Stop song command while not in channel.')
             return message.channel.send(`${message.author} **You need to be in a channel to execute this command!**`);
         }
-        console.log('Left the channel.')
+        if(!server_queue){
+            return voice_channel.leave();
+        } else {
         server_queue.songs = [];
-        server_queue.connection.dispatcher.end();
+        server_queue.connection.dispatcher.end();      
+        }
     }
 };
