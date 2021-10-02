c4bc72917d81f82ff2d4a38d179fa97d766b80af
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-03 00:50:14 +0200

    support timeout update

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -25,39 +25,39 @@ module.exports = {
         let current = 0
 
         console.log(currentSongTitle)
-            let res = await lyricsFinder(singer, currentSongTitle) || "Not Found"
+        let res = await lyricsFinder(singer, currentSongTitle) || "Not Found"
 
-            for(let i = 0; i < res.length; i += 2048) {
-                let lyrics = res.substring(i, Math.min(res.length, i + 2048))
-                let page = new discord.MessageEmbed()
-                .setDescription(lyrics)
-                pages.push(page)
-            }
+        for(let i = 0; i < res.length; i += 2048) {
+            let lyrics = res.substring(i, Math.min(res.length, i + 2048))
+            let page = new discord.MessageEmbed()
+            .setDescription(lyrics)
+            pages.push(page)
+        }
 
-            const filter2 = (reaction, user) => ['??','??'].includes(reaction.emoji.name) && (message.author.id == user.id)
-            const Embed = await message.channel.send(`**Page: ${current+1}/${pages.length}**`, pages[current])
-            await Embed.react('??')
-            await Embed.react('??')
+        const filter2 = (reaction, user) => ["??","??"].includes(reaction.emoji.name) && (message.author.id == user.id)
+        const Embed = await message.channel.send(`**Page: ${current+1}/${pages.length}**`, pages[current])
+        await Embed.react("??")
+        await Embed.react("??")
 
-            let ReactionCol = Embed.createReactionCollector(filter2)
+        let ReactionCol = Embed.createReactionCollector(filter2)
 
-            ReactionCol.on("collect", (reaction, user) => {
-                reaction.users.remove(reaction.users.cache.get(message.author.id))
+        ReactionCol.on("collect", (reaction, user) => {
+            reaction.users.remove(reaction.users.cache.get(message.author.id))
 
-                if(reaction.emoji.name === '??') {
-                    if(current < pages.length - 1) {
-                        current += 1
+            if(reaction.emoji.name === "??") {
+                if(current < pages.length - 1) {
+                    current += 1
+                    Embed.edit(`Page: ${current+1}/${pages.length}`, pages[current])
+                }
+            } else {
+                if(reaction.emoji.name === "??") {
+                    if(current !== 0) {
+                        current -= 1
                         Embed.edit(`Page: ${current+1}/${pages.length}`, pages[current])
                     }
-                } else {
-                    if(reaction.emoji.name === '??') {
-                        if(current !== 0) {
-                            current -= 1
-                            Embed.edit(`Page: ${current+1}/${pages.length}`, pages[current])
-                        }
-                    }
                 }
-            })
+            }
+        })
         
     }
 }
--- commands/support.js
+++ commands/support.js
@@ -1,67 +1,78 @@
 const Discord = require("discord.js");
 
+const talkedRecently = new Set();
 module.exports = {
-    name: 'support',
-    description: 'open a support ticket!',
-    aliases: [],
-    permissions: [], 
-    cooldown: 3600000,
-    async execute(message, args, client) {
-        const channel = await message.guild.channels.create(`ticket: ${message.author.username}`);
-    
-    channel.setParent("892829064564473886");
+  name: 'support',
+  description: 'open a support ticket!',
+  aliases: [],
+  permissions: [],
+  async execute(message, args, client) {
+    if (talkedRecently.has(message.author.id)) {
+      message.channel.send(`**You can only use this command once, wait 1 hour!** ` + `***${message.author}***`);
+    } else {
+      const channel = await message.guild.channels.create(`ticket: ${message.author.username}`);
 
-    channel.updateOverwrite(message.guild.id, {
-      SEND_MESSAGE: false,
-      VIEW_CHANNEL: false,
-    });
-    channel.updateOverwrite(message.author, {
-      SEND_MESSAGE: true,
-      VIEW_CHANNEL: true,
-    });
+      channel.setParent("892829064564473886");
 
-    const reactionEmbed = new Discord.MessageEmbed()
-            .setColor('0x03f4fc')
-            .setTitle('Welcome!')
-            .setDescription('**Hi!\n Please describe your problem.\n Our staff will reach out to you asap!**')
-            .setFooter('If you think your problem is solved, please react to with 🔒 or ⛔');
-
-    const reactionMessage = await channel.send(reactionEmbed);
-
-    try {
-      await reactionMessage.react("🔒");
-      await reactionMessage.react("⛔");
-    } catch (err) {
-      channel.send("Error sending emojis!");
-      throw err;
-    }
-
-    const collector = reactionMessage.createReactionCollector(
-      (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR"),
-      { dispose: true }
-    );
-
-    collector.on("collect", (reaction, user) => {
-      switch (reaction.emoji.name) {
-        case "🔒":
-          channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
-          channel.send("**Ticket owner just closed an issue!**");
-          break;
-        case "⛔":
-          channel.send("**Channel will be deleted in 5 seconds.**");
-          setTimeout(() => channel.delete(), 5000);
-          break;
-      }
-    });
-
-    message.channel
-      .send(`Your ticket is created! ${channel}`)
-      .then((msg) => {
-        setTimeout(() => msg.delete(), 7000);
-        setTimeout(() => message.delete(), 3000);
-      })
-      .catch((err) => {
-        throw err;
+      channel.updateOverwrite(message.guild.id, {
+        SEND_MESSAGE: false,
+        VIEW_CHANNEL: false,
       });
-    },
+      channel.updateOverwrite(message.author, {
+        SEND_MESSAGE: true,
+        VIEW_CHANNEL: true,
+      });
+
+      const reactionEmbed = new Discord.MessageEmbed()
+        .setColor('0x03f4fc')
+        .setTitle('Welcome!')
+        .setDescription('**Hi!\n Please describe your problem.\n Our staff will reach out to you asap!**')
+        .setFooter('If you think your problem is solved, please react to with 🔒 or ⛔');
+
+      const reactionMessage = await channel.send(reactionEmbed);
+
+      try {
+        await reactionMessage.react("🔒");
+        await reactionMessage.react("⛔");
+      } catch (err) {
+        channel.send("Error sending emojis!");
+        throw err;
+      }
+
+      const collector = reactionMessage.createReactionCollector(
+        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR"),
+        { dispose: true }
+      );
+
+      collector.on("collect", (reaction, user) => {
+        switch (reaction.emoji.name) {
+          case "🔒":
+            channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
+            channel.send("**Ticket owner just closed an issue!**");
+            break;
+          case "⛔":
+            channel.send("**Channel will be deleted in 5 seconds.**");
+            setTimeout(() => channel.delete(), 5000);
+            break;
+        }
+      });
+
+      message.channel
+        .send(`Your ticket is created! ${channel}`)
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 7000);
+          setTimeout(() => message.delete(), 3000);
+        })
+        .catch((err) => {
+          throw err;
+        });
+
+        //setting the timeout for 1 hour so that the user can only run this command once and then will have to wait 
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 3600000);
+    }
+  },
 };
