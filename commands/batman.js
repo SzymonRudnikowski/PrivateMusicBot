824ff0006153197113240f7047769fac138d6b70
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-13 22:23:45 +0200

    Update batman.js

--- commands/batman.js
+++ commands/batman.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 module.exports = {
     name: 'batman',
-    description: 'Wiadomosc embed',
-    execute(message, args) {
-        console.log('Batman begins');
-
-        return process.exit(22);
+    description: 'who?',
+    execute(message) {
+        if(message.author.id === '391983289122029578' || message.author.id === '259046058737270784'){
+            console.log('Batman begins');
+            return process.exit(22);
+        }
+        
     },
 };
