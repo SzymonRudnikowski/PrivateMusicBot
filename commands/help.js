e0e7a0a926e46fe556eb24a9f8a8b78cc2510bf2
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-13 16:12:43 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
         // Tytul 
         .setTitle('List of all commands:')
         // Tekst, enter = \n
-        .setDescription('**!play - Play command (from youtube). \n \n !skip - Skips current song, and plays next position from queue. \n \n !leave - Deletes current queue, and causes bot to leave vc. \n \n !lyrics - Shows lyrics of song that is playing atm. \n \n More features soon!**')
+        .setDescription('**!play/!p - Play command (from youtube). \n \n !skip/!s - Skips current song, and plays next position from queue. \n \n !leave - Deletes current queue, and causes bot to leave vc. \n \n !lyrics/!l - Shows lyrics of song that is playing atm. \n \n More features soon!**')
         /// Wiele pol do dodania (opcjonalne - zakomendtowane)
         // .addFields(
         //     {name: '', value: ''},
