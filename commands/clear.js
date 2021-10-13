8cfb1dea164420b0feb2633f968d4318e2b86f37
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-13 21:40:55 +0200

    Create clear.js

---
+++ commands/clear.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'clear',
+    aliases: ['c'],
+    decription: "deletes all message in a channel",
+    async execute(message) {
+        let fetched;
+        do {
+            fetched = await channel.fetchMessages({limit: 100});
+            message.channel.bulkDelete(fetched);
+        }
+        while(fetched.size >= 2);
+    }
+};
