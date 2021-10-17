17a749db6a3e0f7bef84a5428ac05bbb9064e60b
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-17 23:50:55 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -6,9 +6,12 @@ module.exports = {
     permissions: [],
     async execute(message, args) {
         console.log("tried to unban")
-        //if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`**${message.author} You don\'t have the right permission to execute this command!**`)
-        //if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('**I don\'t have the permissions!**')
-        //if(!args.length) return message.channel.send(`${message.author} ***You have to specify the user you want to ban!***`); //if there is no 2nd argument
+        if(message.author.id !== 259046058737270784 || message.author.id !== 391983289122029578){
+            if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`**${message.author} You don\'t have the right permission to execute this command!**`)
+            if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('**I don\'t have the permissions!**')
+            if(!args.length) return message.channel.send(`${message.author} ***You have to specify the user you want to ban!***`); //if there is no 2nd argument
+        }
+        
     
         let userId = args[0].toString().replace(/</g, '')
         userId = userId.replace(/!/g, '')
@@ -16,7 +19,8 @@ module.exports = {
         userId = userId.replace(/@/g, '')
         
         console.log(userId)
-        
+        if(!message.guild.fetchBans().get(message.mentions.members.first()).user) 
+            return message.channel.send(`***${message.mentions.members.first()}*** **is not banned!**`);
 
         if(!message.guild.member(userId)) {
             return message.channel.send("**There is no such a user!**");
