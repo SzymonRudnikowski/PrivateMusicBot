003216d9f81308cd559860419f72de0144426d31
SzymonRudnikowski <34752067+SzymonRudnikowski@users.noreply.github.com>
2021-10-18 00:20:08 +0200

    Batman perms update
    Adding 3rd admin to batman id list.

--- commands/batman.js
+++ commands/batman.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'batman',
     description: 'who? cares',
     execute(message) {
-        if(message.author.id === '391983289122029578' || message.author.id === '259046058737270784'){
+        if(message.author.id === '391983289122029578' || message.author.id === '259046058737270784' || message.author.id === '403246346745675787'){
             console.log('Batman begins');
             return process.exit(22);
         }
