170cd957234f242a53b9f8771383758ec798d4ee
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-19 13:15:56 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -23,7 +23,7 @@ module.exports = {
           
             const bannedUser = banList.find(user => user.id === userId);
           
-            if(!bannedUser){
+            if(!bannedUser && bannedUser !== undefined){
                 console.log(`${message.mentions.members.first()} is not banned!`)
                 return message.channel.send(`***${message.mentions.members.first()}*** ** is not banned!**`);
             }
