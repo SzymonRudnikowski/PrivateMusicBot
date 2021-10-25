2ec68173482d70801828b75ec21b88a55d2421d9
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-25 23:57:45 +0200

    .

--- commands/stoploop.js
+++ commands/stoploop.js
@@ -9,6 +9,7 @@ module.exports = {
         const voice_channel = message.member.voice.channel;
         if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
         if(songTitles.length === 1) return message.channel.send("**No music is currently played!**");
+        if(!looped) return message.channel.send("**Nothing is looped!**");
         looped = false;
         console.log("unlooped using stoploop! status: " + looped)
         return message.channel.send("**Loop ended!**");
