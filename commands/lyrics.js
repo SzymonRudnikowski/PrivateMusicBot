03e1fa489f76a040fcf4a1572e1ebc331439d44b
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 15:05:50 +0200

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -56,7 +56,7 @@ const displayLyrics = async (pages, singer, songTitle, message) => {
     if(songTitle === "") return message.channel.send("**No music is currently played!**");
     let current = 0
     console.log("current song title: " + songTitle)
-    let res = await lyricsFinder(singer, songTitle) || "Not Found"
+    let res = await lyricsFinder(singer, songTitle) || "Not Found or Google is blocking the connection"
 
     for(let i = 0; i < res.length; i += 2048) {
         let lyrics = res.substring(i, Math.min(res.length, i + 2048))
@@ -109,7 +109,7 @@ const displayLyrics = async (pages, singer, songTitle, message) => {
 const displayLyricsNoPlay = async (pages, singer, songTitle, message) => {
     let current = 0
     console.log("current song title no play: " + songTitle)
-    let res = await lyricsFinder(singer, songTitle) || "Not Found"
+    let res = await lyricsFinder(singer, songTitle) || "Not Found or Google is blocking the connection"
 
     for(let i = 0; i < res.length; i += 2048) {
         let lyrics = res.substring(i, Math.min(res.length, i + 2048))
