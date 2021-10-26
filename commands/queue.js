1d371d785cfe2f3e1d6da9cd1a83157cdcae5d78
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 02:57:52 +0200

    queue display added

---
+++ commands/queue.js
@@ -0,0 +1,32 @@
+const discord = require('discord.js')
+const songTit = require("./play")
+
+module.exports = {
+    name: 'queue',
+    aliases: ['q'],
+    description: 'shows first 10 songs in the queue',
+    async execute(message){
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
+        try{
+            if(!server_queue){
+
+            }
+        }catch(err){
+            console.log("queue empty cant show");
+            return message.channel.send("**Queue is empty!**");
+        }
+        let text = ""
+        for(let i = 0; i < 10; i++){
+            if(i === server_queue.songs.length) break;
+            text += server_queue.songs[i] + '\n'
+        }
+        const embed = new Discord.MessageEmbed()
+        .setColor('0x03f4fc')
+        .setTitle('Queue')
+        .setDescription(text)
+        .setFooter('PMB');
+
+        message.channel.send(embed);
+    }
+}
