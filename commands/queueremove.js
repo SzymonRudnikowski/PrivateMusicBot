db3cb5bf82b5ec83134a84931848e28f620c8682
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 17:07:48 +0200

    Update queueremove.js

--- commands/queueremove.js
+++ commands/queueremove.js
@@ -9,8 +9,8 @@ module.exports = {
         if(!args.length) return message.channel.send(`${message.author} **You need to specify position in queue to remove!**`);
 
         try{
-            let removedTitle = server_queue.songs[args[0]+1]
-            server_queue.songs.splice(args[0]+1, 1);
+            let removedTitle = server_queue.songs[args[0]]
+            server_queue.songs.splice(args[0], 1);
             console.log("removed an item from queue")
             return message.channel.send("***" + removedTitle + "*** **has been successfully removed from the queue!**");
         }catch(err){
