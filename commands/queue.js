be64019d9a73962576174b23b341a8dba1310096
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 03:12:26 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -12,7 +12,7 @@ module.exports = {
             let text = ""
             for(let i = 0; i < 10; i++){
                 if(i === server_queue.songs.length) break;
-                text += '%d' + '. ' + server_queue.songs[i] + '\n', i+1
+                text += (i+1) + '. ' + server_queue.songs[i] + '\n'
             }
             const embed = new Discord.MessageEmbed()
             .setColor('0x03f4fc')
