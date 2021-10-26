24c7aaaa6aba785ecf45b32504cc8a17c9e01b97
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 17:04:24 +0200

    Create queueremove.js

---
+++ commands/queueremove.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'queueremove',
+    aliases: ['queue_remove', 'qr'],
+    decription: "skips song that is currently playing in the queue",
+    async execute(message, args) {
+        if (!message.member.voice.channel) return message.channel.send(`${message.author} **You need to be in a channel to execute this command!**`);
+        if(!args.length) return message.channel.send(`${message.author} **You need to specify position in queue to remove!**`);
+
+        try{
+            let removedTitle = server_queue.songs[args[0]+1]
+            server_queue.songs.splice(args[0]+1, 1);
+            console.log("removed an item from queue")
+            return message.channel.send("***" + removedTitle + "*** **has been successfully removed from the queue!**");
+        }catch(err){
+            console.log("tried to remove song from empty queue")
+        }
+    }
+};
