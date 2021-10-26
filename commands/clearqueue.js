78085ceda2aa246421d8ef576c5cdf0facf61a12
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-26 02:28:53 +0200

    Create clearqueue.js

---
+++ commands/clearqueue.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js')
+const songTit = require("./play")
+
+module.exports = {
+    name: 'clearqueue',
+    aliases: ['cq', 'clearq'],
+    description: 'clears song queue',
+    async execute(message){
+        const voice_channel = message.member.voice.channel;
+        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
+        if(!server_queue) return message.channel.send("**There is nothing to clear - the queue is empty!**");
+        
+        queue.delete(message.guild.id);
+        return message.channel.send("**Queue has been successfully cleared!**");
+    }
+}
