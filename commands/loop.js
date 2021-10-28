4f5213a134725e7dc436cfd221841d83bed52e02
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-10-28 20:20:08 +0200

    .

--- commands/loop.js
+++ commands/loop.js
@@ -10,7 +10,7 @@ module.exports = {
     async execute(message){
         if(!looped.has(message.guild.id)) looped.set(message.guild.id, false)
         const voice_channel = message.member.voice.channel;
-        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
+        if (!voice_channel) return message.channel.send('**You need to be in a channel to execute this command!**');
         if(songTitles.get(message.guild.id).length === 1 || YoutubeTitle.get(message.guild.id).length === 1) return message.channel.send("**No music is currently played!**");
         if(looped.get(message.guild.id)) return message.channel.send("***" + YoutubeTitle.get(message.guild.id)[1] + "*** **is already in a loop!**");
         looped.set(message.guild.id, true);
--- commands/stoploop.js
+++ commands/stoploop.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'end looped song',
     async execute(message){
         const voice_channel = message.member.voice.channel;
-        if (!voice_channel) return message.channel.send('You need to be in a channel to execute this command!');
+        if (!voice_channel) return message.channel.send('**You need to be in a channel to execute this command!**');
         if(songTitles.length === 1) return message.channel.send("**No music is currently played!**");
         if(!looped.get(message.guild.id)) return message.channel.send("**Nothing is looped!**");
         looped.set(message.guild.id, false)
