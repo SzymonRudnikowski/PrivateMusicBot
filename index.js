712b92660bece21afef757ff9b64891987b19d45
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2021-12-23 03:42:37 +0100

    fully functional anti spam added

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.aliases = new Discord.Collection();
-global.commandUsedRecently = new Map();
-global.mutedUsers = new Set();
-
+let commandUsedRecently = new Map();
+let mutedUsers = new Map();
+let mutedUsersCurrently = new Set();
+let intervals = [5000, 30000, 60000, 300000, 1800000, 3600000, 10800000, 43200000, 86400000]
+            //    5s    30s    60s    5min    30min    1hour    3hours    12hours   24hours
 const BOT_ID = "892442837252206633";
 //btw simon is a ni33er
 
@@ -48,18 +50,37 @@ for(const file of commandFiles) {
 // }, 10000);
 
 client.on("message", message => {
-  if(!message.content.startsWith(prefix) || mutedUsers.has(message.author.id)) return;
+  if(!message.content.startsWith(prefix) || mutedUsersCurrently.has(message.author.id)) return;
 
   if(!commandUsedRecently.has(message.author.id)) commandUsedRecently.set(message.author.id, 1);
 
+  setTimeout(() => {
+    mutedUsers.clear();
+  },86400000); //clear mute stage every day
+
   if(commandUsedRecently.get(message.author.id) === 3){
-      message.channel.send('**You are sending messages to fast! Try again in 60 seconds**');
-      mutedUsers.add(message.author.id);
+      console.log(mutedUsers.has(message.author.id));
+      if(mutedUsers.has(message.author.id)) mutedUsers.set(message.author.id, mutedUsers.get(message.author.id) + 1);
+      else mutedUsers.set(message.author.id, 0);
+      console.log(mutedUsers.has(message.author.id));
+
+      if(intervals[mutedUsers.get(message.author.id)]/1000 <= 60)
+        message.channel.send(`**You are sending messages to fast! Try again in ${(intervals[mutedUsers.get(message.author.id)]/1000)} seconds**`);
+      else if(intervals[mutedUsers.get(message.author.id)]/1000 > 60 && intervals[mutedUsers.get(message.author.id)]/1000 <= 1800)
+        message.channel.send(`**You are sending messages to fast! Try again in ${(intervals[mutedUsers.get(message.author.id)]/1000/60)} minutes**`);
+      else if(intervals[mutedUsers.get(message.author.id)]/1000 === 3600)  
+        message.channel.send(`**You are sending messages to fast! Try again in ${(intervals[mutedUsers.get(message.author.id)]/1000/60/60)} hour**`);
+      else if(intervals[mutedUsers.get(message.author.id)]/1000 > 1800)
+        message.channel.send(`**You are sending messages to fast! Try again in ${(intervals[mutedUsers.get(message.author.id)]/1000/60/60)} hours**`);
+
+      mutedUsersCurrently.add(message.author.id);
+
       setTimeout(() => {
-        // Removes the user from the set after 60 secs
-        mutedUsers.delete(message.author.id);
-      }, 60000);
-      console.log(`${message.author} is spamming, blocked for 60 secs`);
+        // Removes the user from the set after proper amount of time
+        mutedUsersCurrently.delete(message.author.id);
+      }, intervals[mutedUsers.get(message.author.id)]);
+
+      console.log(`${message.author} is spamming, blocked`);
       return;
   }
   else{
