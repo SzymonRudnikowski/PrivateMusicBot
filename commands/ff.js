1c657647527e90fefcf5494e88ff99a6ddeac0dd
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-01-26 20:01:34 +0100

    .

---
+++ commands/ff.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require('discord.js')
+const fs = require('fs')
+const fetch = require("node-fetch");
+const XLSX = require('xlsx');
+
+module.exports = {
+    name: 'ff',
+    aliases: [],
+    async execute(message, args, com, client) {
+
+        //!ff nazwa_team1/nazwa_team2 
+
+    },
+};
