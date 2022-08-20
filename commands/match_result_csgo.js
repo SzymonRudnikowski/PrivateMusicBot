386ef649fb5251618818540689e68ee35eced832
Olaf Dalach <dalach.olaf@gmail.com>
2022-08-21 00:02:45 +0200

    .

--- commands/match_result_csgo.js
+++ commands/match_result_csgo.js
@@ -11,18 +11,28 @@ module.exports = {
 			return message.channel.send(messEmbednow);
 		}
 		const input = args.join(' ').split(';'); // team name, score, score, team name
+		const team1 = input[0];
+		const team2 = input[3];
 
 		const result = {
-			[input[0]]: input[1],
-			[input[3]]: input[2],
+			[team1]: input[1],
+			[team2]: input[2],
 		};
 
-		fs.readFile(`./MLE/match_results_csgo.txt`, 'utf-8', (err, data) => {
-			if (err) {
-				console.log('Error while reading the file');
-			} else {
+		fs.readFile(`./MLE/match_results_csgo.txt`, 'utf-8')
+			.then((data) => {
 				let matchResults = JSON.parse(data.toString());
+				matchResults.forEach((match_result) => {
+					if (match_result[team1] && match_result[team2]) {
+						throw 0;
+					} else if (match_result[team1]) {
+						throw 1;
+					} else if (match_result[team2]) {
+						throw 2;
+					}
+				});
 				matchResults.push(result);
+
 				const return_string = JSON.stringify(matchResults, null, 4);
 				fs.writeFile(`./MLE/match_results_csgo.txt`, return_string, (err) => {
 					if (err) {
@@ -30,11 +40,34 @@ module.exports = {
 					} else {
 						console.log('match results csgo have been uploaded');
 					}
+				}).then(() => {
+					const messEmbednow = new MessageEmbed().setTitle(`**Match results have been successfully uploaded**`).setColor('GREEN').setTimestamp();
+					return message.channel.send(messEmbednow);
 				});
-			}
-		}).then(() => {
-			const messEmbednow = new MessageEmbed().setTitle(`**Match results have been successfully uploaded**`).setColor('GREEN').setTimestamp();
-			return message.channel.send(messEmbednow);
-		});
+			})
+			.catch((errCode) => {
+				if (errCode === 0) {
+					const messEmbednow = new MessageEmbed()
+						.setTitle(`**Match results for** ***${team1}*** **and** ***${team2}*** **have already been uploaded!**`)
+						.setColor('RED')
+						.setTimestamp();
+					return message.channel.send(messEmbednow);
+				} else if (errCode === 1) {
+					const messEmbednow = new MessageEmbed()
+						.setTitle(`**Match results for** ***${team1}*** **have already been uploaded!**`)
+						.setColor('RED')
+						.setTimestamp();
+					return message.channel.send(messEmbednow);
+				} else if (errCode === 2) {
+					const messEmbednow = new MessageEmbed()
+						.setTitle(`**Match results for** ***${team2}*** **have already been uploaded!**`)
+						.setColor('RED')
+						.setTimestamp();
+					return message.channel.send(messEmbednow);
+				} else {
+					const messEmbednow = new MessageEmbed().setTitle(`**An error occurred while uploading the results!**`).setColor('RED').setTimestamp();
+					return message.channel.send(messEmbednow);
+				}
+			});
 	},
 };
