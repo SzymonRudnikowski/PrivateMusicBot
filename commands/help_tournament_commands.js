504fbec9596cb7d4de6832930464682148f06558
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-09-17 17:37:37 +0200

    .

--- commands/help_tournament_commands.js
+++ commands/help_tournament_commands.js
@@ -1,30 +1,43 @@
 const Discord = require('discord.js');
 module.exports = {
-	name: 'help_mle',
-	description: 'Help commands for the MLE server',
-	execute(message, args) {
-		if (message.guild.id !== '914969283661037618') return;
-		const help = new Discord.MessageEmbed()
-			.setColor('0x03f4fc')
-			.setTitle('List of commands for the MLE server:')
-			.addFields(
-				{
-					name: '***Counter-Strike: Global Offensive***',
-					value: '!walkover_csgo name winning team/name losing team - win/lose by walkover (admin only) \n \n !current_queue_csgo - displays current queue number \n \n !set_queue_csgo - sets the number of the current queue (admin only) \n \n !resume_stats_csgo - enables sending game stats (admin only) \n \n !stop_stats_csgo - disables sending game stats (admin only) \n \n !score_csgo - sends stats of the game and updates the database \n \n !win_csgo name winning team - every player from given team gets 3 kills (admin only) \n \n !match_result_csgo team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw both, teams lost i.e. 0;0)',
-				},
-				{ name: '\u200B', value: '\u200B' },
-				{
-					name: '***League of Legends***',
-					value: '!walkover_lol name winning team/name losing team - win/lose by walkover (admin only) \n \n !current_queue_lol - displays current queue number \n \n !set_queue_lol - sets the number of the current queue (admin only) \n \n !resume_stats_lol - enables sending game stats (admin only) \n \n !stop_stats_lol - disables sending game stats (admin only) \n \n !score_lol - sends stats of the game and updates the database \n \n !win_lol name winning team - every player from given team gets 3 kills and 5.0 cs/min (admin only) \n \n !match_result_lol team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw both, teams lost i.e. 0;0)',
-				},
-				{ name: '\u200B', value: '\u200B' },
-				{
-					name: '***Valorant***',
-					value: '!match_result_val team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw, both teams lost i.e. 0;0)',
-				}
-			)
-			.setFooter('PMB - v1.0');
+  name: 'help_mle',
+  description: 'Help commands for the MLE server',
+  execute(message, args) {
+    if (message.guild.id !== '914969283661037618') return;
+    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(
+          `***${message.author.tag}*** **you don't have permissions to execute this command!**`
+        )
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    }
 
-		return message.channel.send(help);
-	},
+    const help = new Discord.MessageEmbed()
+      .setColor('0x03f4fc')
+      .setTitle('List of commands for the MLE server:')
+      .addFields(
+        {
+          name: '***Counter-Strike: Global Offensive***',
+          value:
+            '!walkover_csgo name winning team/name losing team - win/lose by walkover (admin only) \n \n !current_queue_csgo - displays current queue number \n \n !set_queue_csgo - sets the number of the current queue (admin only) \n \n !resume_stats_csgo - enables sending game stats (admin only) \n \n !stop_stats_csgo - disables sending game stats (admin only) \n \n !score_csgo - sends stats of the game and updates the database \n \n !win_csgo name winning team - every player from given team gets 3 kills (admin only) \n \n !match_result_csgo team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw both, teams lost i.e. 0;0)',
+        },
+        { name: '\u200B', value: '\u200B' },
+        {
+          name: '***League of Legends***',
+          value:
+            '!walkover_lol name winning team/name losing team - win/lose by walkover (admin only) \n \n !current_queue_lol - displays current queue number \n \n !set_queue_lol - sets the number of the current queue (admin only) \n \n !resume_stats_lol - enables sending game stats (admin only) \n \n !stop_stats_lol - disables sending game stats (admin only) \n \n !score_lol - sends stats of the game and updates the database \n \n !win_lol name winning team - every player from given team gets 3 kills and 5.0 cs/min (admin only) \n \n !match_result_lol team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw both, teams lost i.e. 0;0)',
+        },
+        { name: '\u200B', value: '\u200B' },
+        {
+          name: '***Valorant***',
+          value:
+            '!match_result_val team_name;score;score;team_name (score - number 0/1 indicating which of the following teams won, if there has been a draw, both teams lost i.e. 0;0)',
+        }
+      )
+      .setFooter('PMB - v1.0');
+
+    return message.channel.send(help);
+  },
 };
--- commands/match_result_val.js
+++ commands/match_result_val.js
@@ -1,85 +1,138 @@
 const discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const fs = require('fs/promises');
+const XLSX = require('xlsx');
+
+const PATH = './MLE/TeamsRegistered.xlsx';
 
 module.exports = {
-	name: 'match_result_val',
-	async execute(message, args, com, client) {
-		if (message.guild.id !== '914969283661037618') return;
-		if (!args || !args.length) {
-			const messEmbednow = new MessageEmbed().setTitle(`**Invalid syntax! Type !help_mle for further details**`).setColor('RED').setTimestamp();
-			return message.channel.send(messEmbednow);
-		}
-		const input = args.join(' ').split(';'); // team name, score, score, team name
-		const team1 = input[0];
-		const team2 = input[3];
+  name: 'match_result_val',
+  async execute(message, args, com, client) {
+    if (message.guild.id !== '914969283661037618') return;
+    if (!args || !args.length) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(`**Invalid syntax! Type !help_mle for further details**`)
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    }
+    const input = args.join(' ').split(';'); // team name, score, score, team name
+    const team1 = input[0];
+    const team2 = input[3];
 
-		if (
-			input.length != 4 ||
-			isNaN(input[1]) ||
-			isNaN(input[2]) ||
-			(input[1] !== '0' && input[1] !== '1') ||
-			(input[2] !== '0' && input[2] !== '1') ||
-			(input[1] === '1' && input[2] === '1')
-		) {
-			const messEmbednow = new MessageEmbed().setTitle(`**Invalid syntax! Type !help_mle for further details**`).setColor('RED').setTimestamp();
-			return message.channel.send(messEmbednow);
-		}
+    if (
+      input.length != 4 ||
+      isNaN(input[1]) ||
+      isNaN(input[2]) ||
+      (input[1] !== '0' && input[1] !== '1') ||
+      (input[2] !== '0' && input[2] !== '1') ||
+      (input[1] === '1' && input[2] === '1')
+    ) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(`**Invalid syntax! Type !help_mle for further details**`)
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    }
 
-		const result = {
-			[team1]: input[1],
-			[team2]: input[2],
-		};
+    const result = {
+      [team1]: input[1],
+      [team2]: input[2],
+    };
 
-		fs.readFile(`./MLE/match_results_val.txt`, 'utf-8')
-			.then((data) => {
-				let matchResults = JSON.parse(data.toString());
-				matchResults.forEach((match_result) => {
-					if (match_result[team1] && match_result[team2]) {
-						throw 0;
-					} else if (match_result[team1]) {
-						throw 1;
-					} else if (match_result[team2]) {
-						throw 2;
-					}
-				});
-				matchResults.push(result);
+    const workbook = XLSX.readFile(PATH);
 
-				const return_string = JSON.stringify(matchResults, null, 4);
-				fs.writeFile(`./MLE/match_results_val.txt`, return_string, (err) => {
-					if (err) {
-						console.log('error while writing the file', err);
-					} else {
-						console.log('match results csgo have been uploaded');
-					}
-				}).then(() => {
-					const messEmbednow = new MessageEmbed().setTitle(`**Match results have been successfully uploaded**`).setColor('GREEN').setTimestamp();
-					return message.channel.send(messEmbednow);
-				});
-			})
-			.catch((errCode) => {
-				if (errCode === 0) {
-					const messEmbednow = new MessageEmbed()
-						.setTitle(`**Match results for** ***${team1}*** **and** ***${team2}*** **have already been uploaded!**`)
-						.setColor('RED')
-						.setTimestamp();
-					return message.channel.send(messEmbednow);
-				} else if (errCode === 1) {
-					const messEmbednow = new MessageEmbed()
-						.setTitle(`**Match results for** ***${team1}*** **have already been uploaded!**`)
-						.setColor('RED')
-						.setTimestamp();
-					return message.channel.send(messEmbednow);
-				} else if (errCode === 2) {
-					const messEmbednow = new MessageEmbed()
-						.setTitle(`**Match results for** ***${team2}*** **have already been uploaded!**`)
-						.setColor('RED')
-						.setTimestamp();
-					return message.channel.send(messEmbednow);
-				} else {
-					const messEmbednow = new MessageEmbed().setTitle(`**An error occurred while uploading the results!**`).setColor('RED').setTimestamp();
-					return message.channel.send(messEmbednow);
-				}
-			});
-	},
+    let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+
+    let team1Found = false;
+    let team2Found = false;
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i][0] === team1) {
+        team1Found = true;
+      } else if (data[i][0] === team2) {
+        team2Found = true;
+      }
+    }
+
+    if (!team1Found && !team2Found) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(`**Teams** ***${team1}*** **and** ***${team2}*** **do not exist!**`)
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    } else if (!team1Found) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(`**Team** ***${team1}*** **does not exist!**`)
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    } else if (!team2Found) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(`**Team** ***${team2}*** **does not exist!**`)
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    }
+
+    fs.readFile(`./MLE/match_results_val.txt`, 'utf-8')
+      .then((data) => {
+        let matchResults = JSON.parse(data.toString());
+        matchResults.forEach((match_result) => {
+          if (match_result[team1] && match_result[team2]) {
+            throw 0;
+          } else if (match_result[team1]) {
+            throw 1;
+          } else if (match_result[team2]) {
+            throw 2;
+          }
+        });
+        matchResults.push(result);
+
+        const return_string = JSON.stringify(matchResults, null, 4);
+        fs.writeFile(`./MLE/match_results_val.txt`, return_string, (err) => {
+          if (err) {
+            console.log('error while writing the file', err);
+          } else {
+            console.log('match results csgo have been uploaded');
+          }
+        }).then(() => {
+          const messEmbednow = new MessageEmbed()
+            .setTitle(`**Match results have been successfully uploaded**`)
+            .setColor('GREEN')
+            .setTimestamp();
+          return message.channel.send(messEmbednow);
+        });
+      })
+      .catch((errCode) => {
+        if (errCode === 0) {
+          const messEmbednow = new MessageEmbed()
+            .setTitle(
+              `**Match results for** ***${team1}*** **and** ***${team2}*** **have already been uploaded!**`
+            )
+            .setColor('RED')
+            .setTimestamp();
+          return message.channel.send(messEmbednow);
+        } else if (errCode === 1) {
+          const messEmbednow = new MessageEmbed()
+            .setTitle(`**Match results for** ***${team1}*** **have already been uploaded!**`)
+            .setColor('RED')
+            .setTimestamp();
+          return message.channel.send(messEmbednow);
+        } else if (errCode === 2) {
+          const messEmbednow = new MessageEmbed()
+            .setTitle(`**Match results for** ***${team2}*** **have already been uploaded!**`)
+            .setColor('RED')
+            .setTimestamp();
+          return message.channel.send(messEmbednow);
+        } else {
+          const messEmbednow = new MessageEmbed()
+            .setTitle(`**An error occurred while uploading the results!**`)
+            .setColor('RED')
+            .setTimestamp();
+          return message.channel.send(messEmbednow);
+        }
+      });
+  },
 };
