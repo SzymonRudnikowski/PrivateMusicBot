2a22d569f17687bb50a80e23207c891b1406d3b8
Olaf Dalach <dalach.olaf@gmail.com>
2022-10-18 19:44:15 +0200

    Update send_custom_codes.js

--- commands/send_custom_codes.js
+++ commands/send_custom_codes.js
@@ -5,14 +5,14 @@ module.exports = {
 	name: 'send_custom_codes',
 	permissions: [],
 	async execute(message, args, com, client) {
-		if (message.guild.id !== '914969283661037618') return;
-		if (!message.member.hasPermission(['ADMINISTRATOR'])) {
-			const messEmbednow = new MessageEmbed()
-				.setTitle(`***${message.author.tag}*** **you don't have permissions to execute this command!**`)
-				.setColor('RED')
-				.setTimestamp();
-			return message.channel.send(messEmbednow);
-		}
+		if (message.guild.id !== '914969283661037618' || message.author.id !== '391983289122029578') return;
+		// if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+		// 	const messEmbednow = new MessageEmbed()
+		// 		.setTitle(`***${message.author.tag}*** **you don't have permissions to execute this command!**`)
+		// 		.setColor('RED')
+		// 		.setTimestamp();
+		// 	return message.channel.send(messEmbednow);
+		// }
 		let teamsCodeData;
 		await fs.readFile(`./MLE/lol-custom-codes/codes.txt`, 'utf-8').then((data) => {
 			teamsCodeData = JSON.parse(data.toString());
