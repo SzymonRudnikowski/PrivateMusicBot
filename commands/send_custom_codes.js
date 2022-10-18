d0fbebb471e188681466f708d58483c06682343c
Olaf Dalach <dalach.olaf@gmail.com>
2022-10-18 18:47:47 +0200

    .

--- commands/send_custom_codes.js
+++ commands/send_custom_codes.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const fs = require('fs/promises');
 
 module.exports = {
@@ -21,17 +21,34 @@ module.exports = {
 		const roleID = '966006170747822131';
 
 		const lolCaptains = await message.guild.roles.cache.get(roleID).members;
+		let codeAppear = new Map();
 
-		lolCaptains.forEach((captain) => {
-			for (let i = 0; i < teamsCodeData.length; i++) {
-				const match = teamsCodeData[i];
-				if (match.team1 === captain.displayName || match.team2 === captain.displayName) {
-					console.log(captain.displayName, match.code);
-					// captain.send(
-					// 	`Hi! I am just here to give you your tournament codes. \n Your opponent have received the same message, enter this code in the *League of Legends* client at the time you and your opponents have agreed on. \n **Code:** ***${match.code}***`
-					// );
+		try {
+			lolCaptains.forEach((captain) => {
+				//console.log('searching for:', captain.displayName);
+				for (let i = 0; i < teamsCodeData.length; i++) {
+					const match = teamsCodeData[i];
+					// if (match.team1 === captain.displayName) {
+					// 	console.log('team1 found:', match.team1);
+					// } else if (match.team2 === captain.displayName) {
+					// 	console.log('team2 found:', match.team2);
+					// }
+					if (match.team1 == captain.displayName || match.team2 == captain.displayName) {
+						// codeAppear.set(match.code, codeAppear.get(match.code) ? codeAppear.get(match.code) + 1 : 1);
+						const messEmbednow = new MessageEmbed()
+							.setTitle(
+								`Hi! I am just here to give you your tournament codes. \n Your opponent have received the same message, enter this code in the *League of Legends* client at the time you and your opponents have agreed on. \n **Code:** ***${match.code}***`
+							)
+							.setColor('BLUE')
+							.setTimestamp();
+						captain.send(messEmbednow);
+					}
 				}
-			}
-		});
+			});
+			const messEmbednow = new MessageEmbed().setTitle('Codes sent successfully').setColor('BLUE').setTimestamp();
+			message.author.send(messEmbednow);
+		} catch (err) {
+			console.log(err);
+		}
 	},
 };
