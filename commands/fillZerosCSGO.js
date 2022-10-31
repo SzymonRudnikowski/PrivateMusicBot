23a66fdc9724c753eaab69836381b9487b71d75d
Ph4sm4 <78483172+Ph4sm4@users.noreply.github.com>
2022-10-31 03:27:37 +0100

    .

--- commands/fillZerosCSGO.js
+++ commands/fillZerosCSGO.js
@@ -10,6 +10,16 @@ module.exports = {
   description: '',
   permissions: [],
   async execute(message, args, com, client) {
+    if (message.guild.id !== '914969283661037618') return;
+    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+      const messEmbednow = new MessageEmbed()
+        .setTitle(
+          `***${message.author.tag}*** **you don't have permissions to execute this command!**`
+        )
+        .setColor('RED')
+        .setTimestamp();
+      return message.channel.send(messEmbednow);
+    }
     endQueueZeroAdd_CSGO(true, 2);
   },
 };
