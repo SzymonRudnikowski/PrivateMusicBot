81ca4112cb6b9b84a68ebdf3bc60d80798f326fb
Olaf Dalach <dalach.olaf@gmail.com>
2022-11-07 19:39:28 +0100

    .

--- commands/fillZerosCSGO.js
+++ commands/fillZerosCSGO.js
@@ -6,66 +6,63 @@ const XLSX = require('xlsx');
 const PATH_CSGO = './MLE/Zawodnicy_CSGO.xlsx';
 
 module.exports = {
-  name: 'fill-zeros-csgo',
-  description: '',
-  permissions: [],
-  async execute(message, args, com, client) {
-    if (message.guild.id !== '914969283661037618') return;
-    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
-      const messEmbednow = new MessageEmbed()
-        .setTitle(
-          `***${message.author.tag}*** **you don't have permissions to execute this command!**`
-        )
-        .setColor('RED')
-        .setTimestamp();
-      return message.channel.send(messEmbednow);
-    }
-    endQueueZeroAdd_CSGO(true, 2);
-  },
+	name: 'fill-zeros-csgo',
+	description: '',
+	permissions: [],
+	async execute(message, args, com, client) {
+		if (message.guild.id !== '914969283661037618') return;
+		if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+			const messEmbednow = new MessageEmbed()
+				.setTitle(`***${message.author.tag}*** **you don't have permissions to execute this command!**`)
+				.setColor('RED')
+				.setTimestamp();
+			return message.channel.send(messEmbednow);
+		}
+		endQueueZeroAdd_CSGO(true, 2);
+	},
 };
 
 function endQueueZeroAdd_CSGO(statsEnabled, queueNumber) {
-  if (!statsEnabled) {
-    console.log('its monday but stats in cs are off');
-    return;
-  }
-  const workbook = XLSX.readFile(PATH_CSGO);
-  let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+	if (!statsEnabled) {
+		console.log('its monday but stats in cs are off');
+		return;
+	}
+	const workbook = XLSX.readFile(PATH_CSGO);
+	let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+	let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
 
-  for (let i = 0; i < data.length; i++) {
-    let array = data[i];
-    console.log(array.length, queueNumber * 5 + 7);
-    if (array.length) {
-      if (array.length === (queueNumber - 1) * 5 + 7) {
-        array[2] += parseInt(0);
-        array[3] += parseInt(0);
-        array[4] += parseInt(0);
+	for (let i = 0; i < data.length; i++) {
+		let array = data[i];
+		console.log(array.length, queueNumber * 5 + 7);
+		if (array.length) {
+			if (array.length === (queueNumber - 1) * 5 + 7) {
+				array[2] += parseInt(0);
+				array[3] += parseInt(0);
+				array[4] += parseInt(0);
 
-        let divideBy = !parseInt(array[4]) ? 1 : parseInt(array[4]);
-        array[5] = (parseInt(array[2]) / divideBy).toFixed(2); //kd
+				let divideBy = !parseInt(array[4]) ? 1 : parseInt(array[4]);
+				array[5] = (parseInt(array[2]) / divideBy).toFixed(2); //kd
 
-        array.push(0);
-        array.push(0);
-        array.push(0);
-        array.push(0);
-        array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0);
 
-        let total_kd = 0;
-        let rounds = 0;
+				let total_kd = 0;
+				let rounds = 0;
 
-        for (let i = 11; i < array.length; i += 5) {
-          total_kd += array[i];
-          rounds++;
-        }
-        array[6] = (parseFloat(total_kd) / parseFloat(rounds)).toFixed(2);
-        console.log(array[1], 'got filled with 0 cause no game was played');
-      }
-    }
-  }
-  let worksheet = XLSX.utils.aoa_to_sheet(data);
-  let new_workbook = XLSX.utils.book_new();
+				for (let i = 11; i < array.length; i += 5) {
+					total_kd += array[i];
+					rounds++;
+				}
+				array[6] = (parseFloat(total_kd) / parseFloat(rounds)).toFixed(2);
+			}
+		}
+	}
+	let worksheet = XLSX.utils.aoa_to_sheet(data);
+	let new_workbook = XLSX.utils.book_new();
 
-  XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
-  XLSX.writeFile(new_workbook, PATH_CSGO);
+	XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
+	XLSX.writeFile(new_workbook, PATH_CSGO);
 }
--- commands/fillZerosLOL.js
+++ commands/fillZerosLOL.js
@@ -6,67 +6,64 @@ const XLSX = require('xlsx');
 const PATH_LOL = './MLE/Zawodnicy_LOL.xlsx';
 
 module.exports = {
-  name: 'fill-zeros-lol',
-  description: '',
-  permissions: [],
-  async execute(message, args, com, client) {
-    if (message.guild.id !== '914969283661037618') return;
-    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
-      const messEmbednow = new MessageEmbed()
-        .setTitle(
-          `***${message.author.tag}*** **you don't have permissions to execute this command!**`
-        )
-        .setColor('RED')
-        .setTimestamp();
-      return message.channel.send(messEmbednow);
-    }
-    endQueueZeroAdd_LOL(true, 2);
-  },
+	name: 'fill-zeros-lol',
+	description: '',
+	permissions: [],
+	async execute(message, args, com, client) {
+		if (message.guild.id !== '914969283661037618') return;
+		if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+			const messEmbednow = new MessageEmbed()
+				.setTitle(`***${message.author.tag}*** **you don't have permissions to execute this command!**`)
+				.setColor('RED')
+				.setTimestamp();
+			return message.channel.send(messEmbednow);
+		}
+		endQueueZeroAdd_LOL(true, 2);
+	},
 };
 
 function endQueueZeroAdd_LOL(statsEnabled, queueNumber) {
-  if (!statsEnabled) {
-    console.log('its monday but stats in lol are off');
-    return;
-  }
-  const workbook = XLSX.readFile(PATH_LOL);
-  let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+	if (!statsEnabled) {
+		console.log('its monday but stats in lol are off');
+		return;
+	}
+	const workbook = XLSX.readFile(PATH_LOL);
+	let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+	let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
 
-  for (let i = 0; i < data.length; i++) {
-    let array = data[i];
-    if (array.length) {
-      if (array.length === 8 + (queueNumber - 1) * 6) {
-        array[2] += parseInt(0);
-        array[3] += parseInt(0);
-        array[4] += parseInt(0);
-        let divideBy = !parseFloat(array[4]) ? 1 : parseFloat(array[4]);
-        array[5] = parseFloat((parseFloat(array[2]) + parseFloat(array[3])) / divideBy).toFixed(2); //KDA
-        array.push(0);
-        array.push(0);
-        array.push(0);
-        array.push(0);
-        array.push(0);
-        array.push(0).toFixed(2);
+	for (let i = 0; i < data.length; i++) {
+		let array = data[i];
+		if (array.length) {
+			if (array.length === 8 + (queueNumber - 1) * 6) {
+				array[2] += parseInt(0);
+				array[3] += parseInt(0);
+				array[4] += parseInt(0);
+				let divideBy = !parseFloat(array[4]) ? 1 : parseFloat(array[4]);
+				array[5] = parseFloat((parseFloat(array[2]) + parseFloat(array[3])) / divideBy).toFixed(2); //KDA
+				array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0);
+				array.push(0).toFixed(2);
 
-        let cs_sum = 0;
-        let kp_sum = 0;
-        let number_of_games = 0;
-        for (let i = 12; i < array.length; i += 6) {
-          cs_sum += parseFloat(array[i]);
-          kp_sum += parseFloat(array[i + 1]);
-          number_of_games++;
-        }
+				let cs_sum = 0;
+				let kp_sum = 0;
+				let number_of_games = 0;
+				for (let i = 12; i < array.length; i += 6) {
+					cs_sum += parseFloat(array[i]);
+					kp_sum += parseFloat(array[i + 1]);
+					number_of_games++;
+				}
 
-        array[6] = parseFloat(cs_sum / number_of_games).toFixed(2);
-        array[7] = parseFloat(kp_sum / number_of_games).toFixed(2);
-        console.log(array[1], 'got filled with 0 cause no game was played');
-      }
-    }
-  }
-  let worksheet = XLSX.utils.aoa_to_sheet(data);
-  let new_workbook = XLSX.utils.book_new();
+				array[6] = parseFloat(cs_sum / number_of_games).toFixed(2);
+				array[7] = parseFloat(kp_sum / number_of_games).toFixed(2);
+			}
+		}
+	}
+	let worksheet = XLSX.utils.aoa_to_sheet(data);
+	let new_workbook = XLSX.utils.book_new();
 
-  XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
-  XLSX.writeFile(new_workbook, PATH_LOL);
+	XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
+	XLSX.writeFile(new_workbook, PATH_LOL);
 }
