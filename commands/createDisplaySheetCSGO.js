fef66c61856a0c7c0afb22c299a6131b379df322
Olaf Dalach <dalach.olaf@gmail.com>
2022-11-07 20:56:41 +0100

    .

--- commands/createDisplaySheetCSGO.js
+++ commands/createDisplaySheetCSGO.js
@@ -9,83 +9,81 @@ const PATH_DISPLAY_CSGO = './MLE/DisplaySheetCSGO.xlsx';
 const PATH_DISPLAY_LOL = './MLE/DisplaySheetLOL.xlsx';
 
 module.exports = {
-  name: 'create-display-sheet-csgo',
-  description: '',
-  permissions: [],
-  async execute(message, args, com, client) {
-    if (message.guild.id !== '914969283661037618') return;
-    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
-      const messEmbednow = new MessageEmbed()
-        .setTitle(
-          `***${message.author.tag}*** **you don't have permissions to execute this command!**`
-        )
-        .setColor('RED')
-        .setTimestamp();
-      return message.channel.send(messEmbednow);
-    }
-    createDisplaySheetCSGO(true, 2);
-  },
+	name: 'create-display-sheet-csgo',
+	description: '',
+	permissions: [],
+	async execute(message, args, com, client) {
+		if (message.guild.id !== '914969283661037618') return;
+		if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+			const messEmbednow = new MessageEmbed()
+				.setTitle(`***${message.author.tag}*** **you don't have permissions to execute this command!**`)
+				.setColor('RED')
+				.setTimestamp();
+			return message.channel.send(messEmbednow);
+		}
+		createDisplaySheetCSGO(true, 3);
+	},
 };
 
 function createDisplaySheetCSGO(statsEnabled, queueNumber) {
-  if (!statsEnabled) {
-    console.log('its monday but stats in csgo display sheet are off');
-    return;
-  }
-  const workbook = XLSX.readFile(PATH_CSGO);
-  let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
+	if (!statsEnabled) {
+		console.log('its monday but stats in csgo display sheet are off');
+		return;
+	}
+	const workbook = XLSX.readFile(PATH_CSGO);
+	let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
+	let data = XLSX.utils.sheet_to_json(first_worksheet, { header: 1 });
 
-  let displayData = [
-    [
-      'Position',
-      'Player',
-      'Team',
-      'Score',
-      'Total Kills',
-      'Total Assists',
-      'Total Deaths',
-      'Total KD',
-      'Total HS%',
-      `${queueNumber}. Kills`,
-      `${queueNumber}. Assists`,
-      `${queueNumber}. Deaths`,
-      `${queueNumber}. KD`,
-      `${queueNumber}. HS%`,
-    ],
-  ];
-  for (let i = 1; i < data.length; i++) {
-    const array = data[i];
-    if (array.length) {
-      let playerData = [-1]; //since position is gonna get changed after sorting everything
-      for (let j = 0; j < 7; j++) {
-        playerData.push(array[j]); //total stats
-      }
-      const temp = playerData[1]; //team name
-      playerData[1] = playerData[2]; //swapping team name with player name
-      playerData[2] = temp;
+	let displayData = [
+		[
+			'Position',
+			'Player',
+			'Team',
+			'Score',
+			'Total Kills',
+			'Total Assists',
+			'Total Deaths',
+			'Total KD',
+			'Total HS%',
+			`${queueNumber}. Kills`,
+			`${queueNumber}. Assists`,
+			`${queueNumber}. Deaths`,
+			`${queueNumber}. KD`,
+			`${queueNumber}. HS%`,
+		],
+	];
+	for (let i = 1; i < data.length; i++) {
+		const array = data[i];
+		if (array.length) {
+			let playerData = [-1]; //since position is gonna get changed after sorting everything
+			for (let j = 0; j < 7; j++) {
+				playerData.push(array[j]); //total stats
+			}
+			const temp = playerData[1]; //team name
+			playerData[1] = playerData[2]; //swapping team name with player name
+			playerData[2] = temp;
 
-      const score = (playerData[3] + playerData[4] / 2) * playerData[6]; // these indexes are proper as we havent yet added score to the array
-      playerData.splice(3, 0, score); //score (total kills + total assists/2) * total kd
+			const score = (playerData[3] + playerData[4] / 2) * playerData[6]; // these indexes are proper as we havent yet added score to the array
+			playerData.splice(3, 0, score); //score (total kills + total assists/2) * total kd
 
-      for (let j = 7 + (queueNumber - 1) * 5; j < 7 + (queueNumber - 1) * 5 + 5; j++) {
-        playerData.push(array[j]); //stats for this particular queue
-      }
-      displayData.push(playerData);
-    }
-  }
+			for (let j = 7 + (queueNumber - 1) * 5; j < 7 + (queueNumber - 1) * 5 + 5; j++) {
+				playerData.push(array[j]); //stats for this particular queue
+			}
+			displayData.push(playerData);
+		}
+	}
 
-  displayData.sort((a, b) => {
-    return b[3] - a[3]; //descending order
-  });
+	displayData.sort((a, b) => {
+		return b[3] - a[3]; //descending order
+	});
 
-  for (let i = 1; i < displayData.length; i++) {
-    displayData[i][0] = i; //position number
-  }
+	for (let i = 1; i < displayData.length; i++) {
+		displayData[i][0] = i; //position number
+	}
 
-  let worksheet = XLSX.utils.aoa_to_sheet(displayData);
-  let new_workbook = XLSX.utils.book_new();
+	let worksheet = XLSX.utils.aoa_to_sheet(displayData);
+	let new_workbook = XLSX.utils.book_new();
 
-  XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
-  XLSX.writeFile(new_workbook, PATH_DISPLAY_CSGO);
+	XLSX.utils.book_append_sheet(new_workbook, worksheet, 'Arkusz1');
+	XLSX.writeFile(new_workbook, PATH_DISPLAY_CSGO);
 }
